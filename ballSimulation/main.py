2722ef4bb1c4ec4515465e1bad1905d89f9199f3
cosmicsid004 <siddharthdream2004@gmail.com>
2025-12-26 14:30:54 +0530

    ballSimulation organized

--- ballSimulation/main.py
+++ ballSimulation/main.py
@@ -1,7 +1,7 @@
 import pygame
-import math
 import tkinter as tk
 from classBall import Ball
+import matplotlib.pyplot as plt
 
 #______________TKINTER___________________________________
 root = tk.Tk()
@@ -28,8 +28,8 @@ window_width = 1280
 window_height = 850
 game_display = pygame.display.set_mode((window_width, window_height))
 
-bg_image = pygame.image.load('background.jpeg').convert()
-ball_image = pygame.image.load('ball.png').convert_alpha()
+bg_image = pygame.image.load('ballSimulation/background.jpeg').convert()
+ball_image = pygame.image.load('ballSimulation/ball.png').convert_alpha()
 ball_image = pygame.transform.scale(ball_image, (150, 150))
 
 clock = pygame.time.Clock() # to limit the FPS
@@ -39,8 +39,7 @@ ball_radius = 50  # got from hit and trial
 
 restitution = 0.6 #bounce 
 friction = 0.85 #ground
-stop_threshold = 30
-air_drag = 0.999
+stop_threshold = 20
 
 PPM = 50 # 50 pixels to 1 meter
 ground_y = 700 # the floor
@@ -90,9 +89,9 @@ while running:
         ball.x = 0 - ball_radius
         ball.vx = -ball.vx * restitution
 
-
     game_display.blit(bg_image, (0, 0)) #Draws (blits) the background image onto the game window at position (0, 0)
     game_display.blit(ball_image, (ball.x, ball.y)) #drawing from sentre
+    # pygame.draw.line(game_display, (0, 0, 0), (ball.x, ball.y), (ball.x + 100, ball.y + 400), 5)
     pygame.display.update() # Updates the entire display to show the latest changes on the screen
     root.update()
 
--- dtForFrame.py
+++
@@ -1,9 +0,0 @@
-v = 0
-u = 10
-g = -9.8
-t = 0.0001
-
-for step in range(100):
-    v += g * t 
-    u += v * t
-    print(f"x: {v}, y: {u}")
